b35c73d0b5ffc5441bc88856935d781f99586337
GUNW0O <kk000624@kyungsung.ac.kr>
2025-04-04 10:25:55 +0900

    읹인증로직와완료

--- src/main/java/shop/mtcoding/blog/user/UserRepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -9,21 +9,29 @@ import org.springframework.stereotype.Repository;
 public class UserRepository {
     private final EntityManager em;
 
-    public void save2(User user) {
+    /*
+        1. createNativeQuery -> 기본쿼리
+        2. createQuery -> JPA가 제공해주는 객체지향 쿼리
+        3. NameQuery -> Query Method는 함수 이름으로 쿼리생성
+        4. EntityGraph ->
+     */
+    public void save(User user) {
         em.persist(user);
     }
 
-    public void save(String username, String password, String email) {
-        em.createNativeQuery("insert into user_tb (username, password, email) values (?, ?, ?)")
-                .setParameter(1, username)
-                .setParameter(2, password)
-                .setParameter(3, email)
-                .executeUpdate();
-    }
 
     public User findByUsername(String username) {
-        return (User) em.createNativeQuery("select * from user_tb where username = ?", User.class)
-                .setParameter(1, username)
+        return em.createQuery("select u from User u where u.username = :username", User.class)
+                .setParameter("username", username)
                 .getSingleResult();
     }
 }
+
+
+//    public void save(String username, String password, String email) {
+//        em.createNativeQuery("insert into user_tb (username, password, email) values (?, ?, ?)")
+//                .setParameter(1, username)
+//                .setParameter(2, password)
+//                .setParameter(3, email)
+//                .executeUpdate();
+//    }
